2074bf4c4fb37255f1e49cdf6d77605e3068e6d2
scriptkid777 <servsystem@yandex.ru>
2024-10-31 21:29:28 +0700

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/org/concurrent/Wget.java
@@ -0,0 +1,16 @@
+package ru.job4j.org.concurrent;
+
+public class Wget {
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> {}
+        );
+        first.start();
+for (int index= 0; index <= 100; index++) {
+    Thread.sleep(200);
+    System.out.print("\rLoading : " + index + "%");
+    Thread.sleep(7);
+}
+System.out.println("\nLoaded.Finish");
+    }
+}
