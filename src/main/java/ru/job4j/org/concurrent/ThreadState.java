9abac5353be6a1341bd676c93d7430de0a625333
scriptkid777 <servsystem@yandex.ru>
2024-10-31 20:05:21 +0700

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/org/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package ru.job4j.org.concurrent;
+import static java.lang.Thread.State.TERMINATED;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(first.getState() + " first");
+        System.out.println(second.getState() + " second");
+        first.start();
+        second.start();
+        while (first.getState() != TERMINATED && second.getState() != TERMINATED) {
+            System.out.println(first.getState() + " first");
+            System.out.println(second.getState() + " second");
+        }
+        System.out.println("Нити остановлены");
+    }
+}
