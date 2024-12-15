da928d7ce50fd935cbd417245cacd789d6d1ab51
scriptkid777 <servsystem@yandex.ru>
2024-12-15 20:18:50 +0700

    3. Денежные переводы AccountStorage [#1104]

---
+++ src/main/java/ru/job4j/org/cash/AccountStorage.java
@@ -0,0 +1,38 @@
+package ru.job4j.org.cash;
+
+import net.jcip.annotations.GuardedBy;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+public class AccountStorage {
+    @GuardedBy("this")
+    private final HashMap<Integer, Account> accounts = new HashMap<>();
+    public  synchronized  boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
+    }
+
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
+    }
+
+    public  synchronized void delete(int id) {
+        accounts.remove(id);
+    }
+
+    public Optional<Account> getById(int id) {
+
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    public boolean transfer(int fromId, int toId, int amount) {
+        Optional<Account> fromID = this.getById(fromId);
+        Optional<Account> toID = this.getById(toId);
+        boolean check = fromID.isEmpty() || toID.isEmpty() || fromID.get().amount() < amount;
+        if (!check) {
+            this.update(new Account(fromID.get().id(), fromID.get().amount() - amount));
+            this.update(new Account(toID.get().id(), toID.get().amount() + amount));
+        }
+        return check;
+    }
+}
---
+++ src/test/java/ru/job4j/org/cash/AccountStorageTest.java
@@ -0,0 +1,76 @@
+package ru.job4j.org.cash;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+class AccountStorageTest {
+
+    @Test
+    void whenAdd() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        assertThat(firstAccount.amount()).isEqualTo(100);
+    }
+
+    @Test
+    void update() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.update(new Account(1, 200));
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        assertThat(firstAccount.amount()).isEqualTo(200);
+    }
+
+    @Test
+    void delete() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.delete(1);
+        assertThat(storage.getById(1)).isEmpty();
+    }
+
+    @Test
+    void transfer() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.add(new Account(2, 100));
+        storage.transfer(1, 2, 100);
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        var secondAccount = storage.getById(2)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        assertThat(firstAccount.amount()).isEqualTo(0);
+        assertThat(secondAccount.amount()).isEqualTo(200);
+    }
+
+    @Test
+    void whenTransferThenSuccessfully() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 300));
+        storage.add(new Account(2, 100));
+        storage.transfer(1, 2, 100);
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        var secondAccount = storage.getById(2)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 2"));
+        assertThat(firstAccount.amount()).isEqualTo(200);
+        assertThat(secondAccount.amount()).isEqualTo(200);
+    }
+
+    @Test
+    void whenNotEnoughFundsThenNothingHappens() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 50));
+        storage.add(new Account(2, 100));
+        storage.transfer(1, 2, 100);
+        var firstAccount = storage.getById(1)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
+        var secondAccount = storage.getById(2)
+                .orElseThrow(() -> new IllegalStateException("Not found account by id = 2"));
+        assertThat(firstAccount.amount()).isEqualTo(50);
+        assertThat(secondAccount.amount()).isEqualTo(100);
+    }
+}
