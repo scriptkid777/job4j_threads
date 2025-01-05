834563780a14bdb8467ee367a662e18817e8d19c
scriptkid777 <servsystem@yandex.ru>
2025-01-06 00:53:24 +0700

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/org/buffer/ParallelSearch.java
@@ -0,0 +1,37 @@
+package ru.job4j.org.buffer;
+
+import ru.job4j.org.queue.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+       final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                        queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        }
+                    }
+                    consumer.interrupt();
+                });
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+    }
+    }
