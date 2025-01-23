1e8f8e59410f8f3769744aa028e286b6fd794214
scriptkid777 <servsystem@yandex.ru>
2025-01-23 22:40:14 +0700

    0. CAS - операции [#6859] #1

--- src/main/java/ru/job4j/org/CASCount.java
+++ src/main/java/ru/job4j/org/CASCount.java
@@ -15,27 +15,4 @@ public class CASCount {
     public int get() {
         return count.get();
     }
-
-    public static void main(String[] args) {
-        CASCount count = new CASCount();
-       Thread thread1 = new Thread(() -> {
-           for (int i = 0; i < 100_000; i++) {
-               count.increment();
-           }
-       });
-       Thread thread2 = new Thread(() -> {
-           for (int i = 0; i < 100_000; i++) {
-               count.increment();
-           }
-       });
-       thread1.start();
-       thread2.start();
-       try {
-           thread1.join();
-           thread2.join();
-       } catch (InterruptedException e) {
-           throw new RuntimeException();
-       }
-        System.out.println(count.get());
-    }
 }
